99391e0f9065b75778a9e04faf278eeae97df1c9
JakubSenderowski <jakubsenderowskica@gmail.com>
2024-12-14 12:40:50 +0100

    Dodanie z kluczem obcym Statystyki.

---
+++ MVVMFirma/Models/BusinessLogic/SalaLiczbaMiejsc.cs
@@ -0,0 +1,32 @@
+﻿using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMFirma.Models.BusinessLogic
+{
+    internal class SalaLiczbaMiejsc:DatabaseClass
+    {
+        #region Kontruktor
+        public SalaLiczbaMiejsc(KinoEntities db)
+            : base(db) { }
+        #endregion
+        #region Funkcje Biznesowe
+        //Funkcja, która zwraca ID towarów, nazwy i kod w KeyAndValue
+        public IQueryable<KeyAndValue> GetSalaLiczbaMiejscKeyAndValueItems()
+        {
+            return (
+                from sala in db.Sala
+                select new KeyAndValue
+                {
+                    Key = sala.SalaID,
+                    Value = sala.LiczbaMiejsc.ToString()
+                }
+                ).ToList().AsQueryable();
+            #endregion
+        }
+    }
+}
--- MVVMFirma/ViewModels/NowaStatystykaViewModel.cs
+++ MVVMFirma/ViewModels/NowaStatystykaViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MVVMFirma.Models.BusinessLogic;
+using MVVMFirma.Models.Entities;
+using MVVMFirma.Models.EntitiesForView;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +9,101 @@ using System.Threading.Tasks;
 
 namespace MVVMFirma.ViewModels
 {
-    public class NowaStatystykaViewModel:WorkspaceViewModel
+    public class NowaStatystykaViewModel:JedenViewModel<Statystyki>
     {
-        public NowaStatystykaViewModel() {
-            base.DisplayName = "Statystyka";
+        #region Konstruktor
+        public NowaStatystykaViewModel()
+           : base("Statystki")
+        {
+            item = new Statystyki();
         }
+        #endregion
+        #region Pola
+        //Dla każdego pola na interface dodajmy properties.
+        public DateTime Data
+        {
+            get
+            {
+                return item.Data;
+            }
+            set
+            {
+                item.Data = value;
+                OnPropertyChanged(() => Data);
+            }
+        }
+        public int LiczbaSprzedanychBiletow
+        {
+            get
+            {
+                return item.LiczbaSprzedanychBiletow;
+            }
+            set
+            {
+                item.LiczbaSprzedanychBiletow = value;
+                OnPropertyChanged(() => LiczbaSprzedanychBiletow);
+            }
+        }
+        public decimal LacznyPrzychod
+        {
+            get
+            {
+                return item.LacznyPrzychod;
+            }
+            set
+            {
+                item.LacznyPrzychod = value;
+                OnPropertyChanged(() => LacznyPrzychod);
+            }
+        }
+        public int? FilmID
+        {
+            get
+            {
+                return item.FilmID;
+            }
+            set
+            {
+                item.FilmID = value;
+                OnPropertyChanged(() => FilmID);
+            }
+        }
+        public int? SalaID
+        {
+            get
+            {
+                return item.SalaID;
+            }
+            set
+            {
+                item.SalaID = value;
+                OnPropertyChanged(() => SalaID);
+            }
+        }
+        #endregion
+        #region WłaściwościDlaComboBoxów
+        public IQueryable<KeyAndValue> FilmyItems
+        {
+            get
+            {
+                return new FilmB(KinoEntities).GetFilmKeyAndValueItems();
+            }
+        }
+        public IQueryable<KeyAndValue> SalaItems
+        {
+            get
+            {
+                return new SalaLiczbaMiejsc(KinoEntities).GetSalaLiczbaMiejscKeyAndValueItems();
+            }
+        }
+        #endregion
+        #region Helpers
+        public override void Save()
+        {
+            KinoEntities.Statystyki.Add(item); //Dodanie towaru do lokalnej kolekcji.
+            KinoEntities.SaveChanges(); //Zapisuje zmiany do bazy danych
+        }
+
+        #endregion
     }
 }
